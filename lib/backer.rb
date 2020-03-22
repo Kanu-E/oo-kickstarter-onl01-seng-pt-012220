eb270ceac972505a623311fa30ec4f8efb162aac
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-03-22 01:43:02 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,10 +3,12 @@ class Backer
 
   def initialize (name)
     @name = name
-    @backed_projects []
+    @backed_projects = []
   end
   
-  
+  def back_project(project)
+     Project.new(project)
+  end
     
     
 end
